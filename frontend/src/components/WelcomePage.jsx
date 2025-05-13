5296d4ea59d17ad858510d841754e22d64138da1
Chris <chrism.12277@gmail.com>
2025-05-12 22:10:07 -0500

    Logins now handled

--- frontend/src/components/WelcomePage.jsx
+++ frontend/src/components/WelcomePage.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const WelcomePage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (location.state && location.state.error) {
+      setError(location.state.error);
+    }
+  }, [location.state]);
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value.trim());
@@ -11,6 +21,40 @@ const WelcomePage = () => {
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value.trim());
+  };
+
+  const handleNavigate = () => {
+    navigate(`/MessagingApp/Account/${username}`);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:8000/login", {
+        method: "POST",
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        setError(await response.text());
+      } else {
+        const { token, message, username } = await response.json();
+        localStorage.setItem("token", token);
+        localStorage.setItem("username", username);
+        console.log(token);
+        console.log(message);
+        alert(message);
+        handleNavigate();
+      }
+    } catch {
+      setError("Error Fetching Backend");
+    }
   };
 
   return (
@@ -36,10 +80,12 @@ const WelcomePage = () => {
           <button
             className="mt-1 w-4/6 bg-indigo-700 text-white p-2 rounded-md hover:bg-indigo-400 transition"
             type="submit"
+            onClick={handleLogin}
           >
             Login
           </button>
         </form>
+        {error != "" && <div className="errorSection">Error: {error}</div>}
         <div className="mt-10">
           <Link
             to="/signup"
