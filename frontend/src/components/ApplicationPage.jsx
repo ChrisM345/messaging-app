ce7c0362e74d9560d1672978f1ee58676db72213
Chris <chrism.12277@gmail.com>
2025-05-19 20:05:53 -0500

    UI for adding friends

--- frontend/src/components/ApplicationPage.jsx
+++ frontend/src/components/ApplicationPage.jsx
@@ -4,10 +4,13 @@ import { useAuth } from "../contexts/AuthContext";
 
 const ApplicationPage = () => {
   // const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
   const [selectedView, setSelectedView] = useState("home");
   const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const [searchUsername, setSearchUsername] = useState("");
 
-  const { setUser } = useAuth();
+  const { user, setUser } = useAuth();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -40,6 +43,36 @@ const ApplicationPage = () => {
       });
   }, []);
 
+  const handleSearchUsername = (e) => {
+    setSearchUsername(e.target.value.trim());
+  };
+
+  const handleAddFriend = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const response = await fetch("http://localhost:8000/sendFriendRequest", {
+        method: "POST",
+        body: JSON.stringify({
+          userId: user.userId,
+          friendUsername: searchUsername,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        setError(await response.text());
+      } else {
+        alert(await response.text());
+        // handleNavigate();
+      }
+    } catch {
+      setError("Error Fetching Backend");
+    }
+  };
+
   return (
     <>
       {!localStorage.getItem("token") ? (
@@ -71,8 +104,35 @@ const ApplicationPage = () => {
               </div>
               <div className="main-content flex-1">
                 {selectedView === "home" && <h1>Welcome to the app!</h1>}
-                {selectedView === "friends" && <h1>Here are your friends</h1>}
-                {selectedView === "friendRequests" && <h1>Pending Friend Requests</h1>}
+                {selectedView === "friends" && (
+                  <>
+                    <h1>Add Friend</h1>
+                    <h2>You can add friends with their username</h2>
+                    <div className="flex items-center gap-2">
+                      <input
+                        onChange={handleSearchUsername}
+                        className="border rounded-md w-4/6 mt-1 p-1 text-center"
+                        value={searchUsername}
+                        type="text"
+                        placeholder="You can add friends with their username."
+                      />
+                      <button
+                        className="mt-1 bg-indigo-700 text-white p-2 rounded-md hover:bg-indigo-400 transition"
+                        type="submit"
+                        onClick={handleAddFriend}
+                      >
+                        Add Friend
+                      </button>
+                    </div>
+                    {error != "" && <div className="errorSection">Error: {error}</div>}
+                    <h1>Here are your friends</h1>
+                  </>
+                )}
+                {selectedView === "friendRequests" && (
+                  <>
+                    <h1>Pending Friend Requests</h1>
+                  </>
+                )}
                 {selectedView === "directMessages" && <h1>Direct Messages</h1>}
               </div>
             </div>
