bda7efb787b20daf39155ceff1d16d11d7f3af0c
Chris <chrism.12277@gmail.com>
2025-05-19 20:05:03 -0500

    Added logout button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,38 @@
-import { Outlet } from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
 import { useAuth } from "./contexts/AuthContext";
 
 export default function App() {
   const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+    navigate("/");
+    window.location.reload();
+  };
 
   return (
     <div className="min-h-screen bg-white text-gray-900">
       <header className="bg-neutral-300 p-3 flex justify-between items-center">
         <h1 className="text-xl font-semibold">Messenger App</h1>
         {user && (
-          <span className="text-sm text-gray-700">
-            Logged in as <strong>{user.username}</strong>
-          </span>
+          <>
+            <span className="text-sm text-gray-700">
+              Logged in as <strong>{user.username}</strong>
+            </span>
+            <button
+              onClick={handleLogout}
+              className="mt-1 bg-indigo-700 text-white p-2 rounded-md hover:bg-indigo-400 transition"
+              type="submit"
+            >
+              Logout
+            </button>
+          </>
         )}
       </header>
-      <div className="flex">
-        <main className="flex flex-1">
+      <div className="">
+        <main className="">
           <Outlet />
         </main>
       </div>
