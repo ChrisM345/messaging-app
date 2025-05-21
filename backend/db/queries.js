432f1d5bbe1addabf669240e06e394dd253b3492
Chris <chrism.12277@gmail.com>
2025-05-20 20:42:35 -0500

    sql queries for getting friend requests

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -20,7 +20,51 @@ async function createUserAccount(username, password) {
   });
 }
 
+async function existingFriend(senderId, receiverId) {
+  const friend = await prisma.friend.findFirst({
+    where: {
+      OR: [
+        { senderId, receiverId },
+        { senderId: receiverId, receiverId: senderId },
+      ],
+    },
+  });
+  return friend;
+}
+
+async function newFriendRequest(senderId, receiverId) {
+  const newFriend = await prisma.friend.create({
+    data: {
+      senderId,
+      receiverId,
+    },
+  });
+  return newFriend;
+}
+
+async function getReceivedFriendRequests(userId) {
+  const receivedFriendRequests = await prisma.friend.findMany({
+    where: {
+      receiverId: userId,
+    },
+  });
+  return receivedFriendRequests;
+}
+
+async function getSentFriendRequests(userId) {
+  const sentFriendRequests = await prisma.friend.findMany({
+    where: {
+      senderId: userId,
+    },
+  });
+  return sentFriendRequests;
+}
+
 module.exports = {
   createUserAccount,
   getUserAccount,
+  existingFriend,
+  newFriendRequest,
+  getReceivedFriendRequests,
+  getSentFriendRequests,
 };
