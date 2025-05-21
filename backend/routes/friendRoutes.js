023d6590df3137edbfe9035be573cf033357277b
Chris <chrism.12277@gmail.com>
2025-05-20 20:43:03 -0500

    added additional routes

--- backend/routes/friendRoutes.js
+++ backend/routes/friendRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const controller = require("../controllers/friendController");
 
 router.post("/sendFriendRequest", controller.sendFriendRequest);
+router.get("/friendRequests/received", controller.receivedFriendRequests);
+router.get("/friendRequests/sent", controller.sentFriendRequests);
 
 module.exports = router;
