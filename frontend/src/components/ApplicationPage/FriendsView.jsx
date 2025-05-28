73573b188e2e421e214b368fe91acbafeddc9a32
Chris <chrism.12277@gmail.com>
2025-05-27 21:10:28 -0500

    fetch and show friends. added option to remove friends

--- frontend/src/components/ApplicationPage/FriendsView.jsx
+++ frontend/src/components/ApplicationPage/FriendsView.jsx
@@ -1,9 +1,10 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 const FriendsView = ({ user }) => {
-  console.log(user);
   const [error, setError] = useState("");
   const [searchUsername, setSearchUsername] = useState("");
+  const [friends, setFriends] = useState([]);
 
   const handleSearchUsername = (e) => {
     setSearchUsername(e.target.value.trim());
@@ -34,9 +35,58 @@ const FriendsView = ({ user }) => {
       setError("Error Fetching Backend");
     }
   };
+
+  const fetchFriends = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/friends?userId=${user.userId}`, {
+        method: "GET",
+      });
+      if (!response.ok) {
+        setError("Error fetching friends");
+      }
+
+      const data = await response.json();
+
+      const cleanedData = data.map((friend) => {
+        const userInfo = friend.senderId === user.userId ? friend.receiver : friend.sender;
+        return {
+          ...userInfo, // spreads username, email, etc.
+          friendId: friend.id, // this is the ID of the friend record
+        };
+      });
+      setFriends(cleanedData);
+    } catch {
+      setError("Failed to fetch friends");
+    }
+  };
+
+  const handleRemoveFriend = async (requestId) => {
+    const action = "delete";
+    try {
+      const response = await fetch("http://localhost:8000/friendRequests/respond", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ requestId, action }),
+      });
+
+      if (!response.ok) {
+        setError("Failed to respond to request");
+      }
+      await fetchFriends();
+    } catch {
+      setError("Failed to update friend request");
+    }
+  };
+
+  useEffect(() => {
+    fetchFriends();
+  }, []);
+
   return (
     <>
-      <h1>Add Friend</h1>
+      <h1 className="text-xl font-semibold mb-4">Add Friend</h1>
       <h2>You can add friends with their username</h2>
       <div className="flex items-center gap-2">
         <input
@@ -55,7 +105,27 @@ const FriendsView = ({ user }) => {
         </button>
       </div>
       {error != "" && <div className="errorSection">Error: {error}</div>}
-      <h1>Here are your friends</h1>
+      <h2 className="text-xl font-semibold mb-4 my-4">Friends List</h2>
+      <div className="space-y-3">
+        {friends.length > 0 ? (
+          friends.map((friend) => (
+            <div
+              key={friend.id}
+              className="flex items-center justify-between bg-white p-4 rounded-lg shadow border border-gray-200"
+            >
+              <p>{friend.username}</p>
+              <button
+                onClick={() => handleRemoveFriend(friend.friendId)}
+                className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded"
+              >
+                Remove Friend
+              </button>
+            </div>
+          ))
+        ) : (
+          <h2>No Friends Yet!</h2>
+        )}
+      </div>
     </>
   );
 };
