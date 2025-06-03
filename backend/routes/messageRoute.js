7bdea65ed332eea70af24a7de6ad146ad25b64ef
Chris <chrism.12277@gmail.com>
2025-06-02 21:59:08 -0500

    message route update to get message history

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controller = require("../controllers/messageController");
 
 router.post("/message/send", controller.sendMessage);
+router.get("/message/history", controller.getMessages);
 
 module.exports = router;
