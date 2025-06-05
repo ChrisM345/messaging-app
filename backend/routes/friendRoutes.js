fdf635d424db02b22c5d97893bf0473e3c34d0ac
Chris <chrism.12277@gmail.com>
2025-06-04 20:07:18 -0500

    protected routes

--- backend/routes/friendRoutes.js
+++ backend/routes/friendRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const verifyToken = require("../middleware/verifyToken");
 
 const controller = require("../controllers/friendController");
 
-router.post("/sendFriendRequest", controller.sendFriendRequest);
-router.get("/friendRequests/received", controller.receivedFriendRequests);
-router.get("/friendRequests/sent", controller.sentFriendRequests);
-router.post("/friendRequests/respond", controller.handleFriendResponse);
-router.get("/friends", controller.handleGetFriends);
+router.post("/sendFriendRequest", verifyToken, controller.sendFriendRequest);
+router.get("/friendRequests/received", verifyToken, controller.receivedFriendRequests);
+router.get("/friendRequests/sent", verifyToken, controller.sentFriendRequests);
+router.post("/friendRequests/respond", verifyToken, controller.handleFriendResponse);
+router.get("/friends", verifyToken, controller.handleGetFriends);
 
 module.exports = router;
--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const verifyToken = require("../middleware/verifyToken");
 
 const controller = require("../controllers/messageController");
 
-router.post("/message/send", controller.sendMessage);
-router.get("/message/history", controller.getMessages);
+router.post("/message/send", verifyToken, controller.sendMessage);
+router.get("/message/history", verifyToken, controller.getMessages);
 
 module.exports = router;
